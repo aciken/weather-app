8156480dbdbafdb31ca4a0a2835bf8b6f6b0a380
aciken <adrianmarton2006@gmail.com>
2024-01-04 11:45:22 +0100

    added image change

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,21 +1,71 @@
+const weatherBack = (function(){
+  const background = {
+    sunny: ['background: linear-gradient(to right, #ffb75e, #ed8f03);', "../dist/images/sun.png"],
+    night: ['background: linear-gradient(to right, #141e30, #243b55)', "../dist/images/night.png"],
+    rain_day: ['background: linear-gradient(to right, #536976, #292e49);', "../dist/images/cloudrain.png"],
+    rain_night: ['background: linear-gradient(to right, #0f2027, #203a43, #2c5364);', "../dist/images/nightrain.png"],
+
+  }
+
+  return {background}
+})();
+
 const apiFunct = () =>{
 
     const searchBtn = document.querySelector('.search-city');
     const cityInput = document.querySelector('.city-input');
-    const displayTemp = document.querySelector('.display-temp');
+    const cityName = document.querySelector('.city-name');
+    const countryName = document.querySelector('.country-name');
+    const currentTemp = document.querySelector('.current-temp');
+    const feelTemp = document.querySelector('.feel-temp');
+    const currentImg = document.querySelector('.current-img');
+    
 
     
 
     searchBtn.addEventListener('click', () =>{
-        let apiKey = 'https://api.weatherapi.com/v1/current.json?key=eb01e213248b4bac8c5151242240301&q='
+        let apiKey = 'http://api.weatherapi.com/v1/forecast.json?key=eb01e213248b4bac8c5151242240301&q='
         apiKey += cityInput.value
+        apiKey += '&days=5&aqi=no&alerts=no'
+        cityInput.value = ''
 
         fetch(apiKey, {
             mode: 'cors'
         })
       .then(res => res.json())
       .then(data => {
-        displayTemp.textContent = data['current'].temp_c;
+        console.log(data)
+        cityName.textContent = data['location'].name;
+        countryName.textContent = data['location'].country;
+
+        currentTemp.textContent = data['current'].temp_c;
+        feelTemp.textContent = data['current'].feelslike_c;
+
+
+        let dateTimeString = data.location.localtime;
+        let parts = dateTimeString.split(' ');
+        let timePart = parts[1];
+        let timeComponents = timePart.split(':'); 
+        let hour = timeComponents[0]; 
+        
+        console.log(hour);
+
+        if(data.current.condition.text.includes('rain')){
+          if(hour > 7 && hour < 18){
+            currentImg.src = weatherBack.background.rain_day[1];
+          } else{
+            currentImg.src = weatherBack.background.rain_night[1];
+          }
+        } else{
+          if(hour > 7 && hour < 18){
+            currentImg.src = weatherBack.background.sunny[1];
+          } else{
+            currentImg.src = weatherBack.background.night[1];
+          }
+        }
+
+
+
       })
     
     
@@ -24,7 +74,10 @@ const apiFunct = () =>{
       });
     })
 
+  }
 
-}
+    
+
+
 
 export default apiFunct
